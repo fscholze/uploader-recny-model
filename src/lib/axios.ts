f107ceae407f69dae3a23097c13a2353139e02a7
FS <f.scholze@gmx.de>
2024-03-29 16:32:13 +0100

    Fix wrong url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,9 +3,9 @@ import axios from 'axios'
 // axiosInstance.defaults.headers['Content-Type'] = 'application/json'
 
 export const axiosInstanceTranscript = axios.create({
-  baseURL: process.env.em + 'transcript'
+  baseURL: process.env.REACT_APP_API_URL_TRANSCRIPT
 })
 
 export const axiosInstanceSlownik = axios.create({
-  baseURL: process.env.REACT_APP_API_URL_SLOWNIK + 'slownik'
+  baseURL: process.env.REACT_APP_API_URL_SLOWNIK
 })
