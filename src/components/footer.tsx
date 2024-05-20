f640416e66e28015a5422cd2999db56493a8bedc
FS <f.scholze@gmx.de>
2024-05-20 18:14:12 +0200

    Make footer a bit nicer for mobile

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,135 +1,101 @@
-import { Box, Link, Stack, Typography } from '@mui/material'
-import { FC } from 'react'
+import { Box, Button, Link, Menu, MenuItem, Typography } from '@mui/material'
+import React, { FC } from 'react'
 import { FOOTER_HEIGHT } from '../types/constants'
 import { ROUTES } from '../routes/default'
 
-export const Footer: FC<{}> = () => (
-  <Box
-    sx={{
-      backgroundColor: 'rgba(255,255,255,0.8)',
-      width: '100%',
-      position: 'absolute',
-      left: 0,
-      bottom: 0,
-      FOOTER_HEIGHT: FOOTER_HEIGHT
-    }}
-  >
-    <Stack direction='row' spacing={12} justifyContent='center' alignItems='center'>
-      <div>
-        <Typography variant='body1'>Žórła:</Typography>
-        <Stack direction='column'>
-          <Typography variant='caption'>
-            Whisper{' '}
-            <Link
-              href='https://github.com/openai/whisper'
-              target='_blank'
-              rel='noopener'
-              color='inherit'
-              underline='hover'
-            >
-              (github)
-            </Link>
-          </Typography>
-          <Typography variant='caption'>
-            Whisper.cpp{' '}
-            <Link
-              href='https://github.com/ggerganov/whisper.cpp'
-              target='_blank'
-              rel='noopener'
-              color='inherit'
-              underline='hover'
-            >
-              (github)
-            </Link>
-          </Typography>
-          <Typography variant='caption'>
-            Fairseq{' '}
-            <Link
-              href='https://github.com/facebookresearch/fairseq'
-              target='_blank'
-              rel='noopener'
-              color='inherit'
-              underline='hover'
-            >
-              (github)
-            </Link>
-          </Typography>
-          <Typography variant='caption'>
-            Žórła aplikacije{' '}
-            <Link
-              href='https://github.com/ZalozbaDev/uploader-recny-model/'
-              target='_blank'
-              rel='noopener'
-              color='inherit'
-              underline='hover'
-            >
-              (github)
-            </Link>
-          </Typography>
-          <Typography variant='caption'>
-            Žórła serwera{' '}
-            <Link
-              href='https://github.com/ZalozbaDev/uploader-recny-model-server/'
-              target='_blank'
-              rel='noopener'
-              color='inherit'
-              underline='hover'
-            >
-              (github)
-            </Link>
-          </Typography>
-        </Stack>
-      </div>
+export const Footer: FC<{}> = () => {
+  const [anchorElZorla, setAnchorElZorla] = React.useState<null | HTMLElement>(null)
+  const [anchorElWotkazy, setAnchorElWotkazy] = React.useState<null | HTMLElement>(null)
+  const openZorla = Boolean(anchorElZorla)
+  const openWotkazy = Boolean(anchorElWotkazy)
+  const handleClickZorla = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorElZorla(event.currentTarget)
+  }
+  const handleClickWotkazy = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorElWotkazy(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorElZorla(null)
+    setAnchorElWotkazy(null)
+  }
 
-      <div>
-        <Typography variant='body1'>Wotkazy:</Typography>
-        <Stack direction='column'>
-          <Typography variant='caption'>
-            Korla Baier HSB modele{' '}
-            <Link
-              href='https://huggingface.co/spaces/Korla/hsb_stt_demo/tree/main'
-              target='_blank'
-              rel='noopener'
-              color='inherit'
-              underline='hover'
-            >
-              (huggingface hub)
-            </Link>
-          </Typography>
-          <Typography variant='caption'>
-            Fraunhofer IKTS{' '}
-            <Link
-              href='https://www.ikts.fraunhofer.de/de/industrieloesungen/akustische_diagnostik/bio_sprachsignale.html'
-              target='_blank'
-              rel='noopener'
-              color='inherit'
-              underline='hover'
-            >
-              (homepage)
-            </Link>
-          </Typography>
-          <Typography variant='caption'>
-            Wuwiće strony{' '}
-            <Link
-              href='https://fs-coding.com/'
-              target='_blank'
-              rel='noopener'
-              color='inherit'
-              underline='hover'
-            >
-              (homepage)
-            </Link>
-          </Typography>
-        </Stack>
-      </div>
-      
+  const getMenuPoint = (text: string, link: string) => (
+    <MenuItem onClick={handleClose}>
+      <Link href={link} target='_blank' rel='noopener' color='inherit' underline='hover'>
+        <Typography variant='caption'>{text}</Typography>
+      </Link>
+    </MenuItem>
+  )
+
+  return (
+    <Box
+      sx={{
+        backgroundColor: 'rgba(255,255,255,0.8)',
+        width: '100%',
+        position: 'absolute',
+        left: 0,
+        bottom: 0,
+        FOOTER_HEIGHT: FOOTER_HEIGHT,
+        flexDirection: 'row'
+      }}
+    >
+      <Button sx={{ color: 'black' }} onClick={handleClickZorla}>
+        <Typography variant='body1'>Žórła ▾</Typography>
+      </Button>
+      <Button sx={{ color: 'black' }} onClick={handleClickWotkazy}>
+        <Typography variant='body1'>Wotkazy ▾</Typography>
+      </Button>
       <Link href={ROUTES.imprint} color='inherit' underline='hover'>
-        Impresum
+        <Button sx={{ color: 'black' }}>
+          <Typography variant='body1'>Impresum</Typography>
+        </Button>
+      </Link>
+      <Link href={ROUTES['datowy-skit']} color='inherit' underline='hover'>
+        <Button sx={{ color: 'black' }}>
+          <Typography variant='body1'>Datowy škit</Typography>
+        </Button>
       </Link>
 
-      <Link href={ROUTES['datowy-skit']} color='inherit' underline='hover'>
-        Datowy škit
-      </Link>
-    </Stack>
-  </Box>
-)
+      <Menu
+        id='basic-menu'
+        anchorEl={anchorElWotkazy}
+        open={openWotkazy}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button'
+        }}
+      >
+        {getMenuPoint(
+          'Korla Baier HSB modele (huggingface hub)',
+          'https://huggingface.co/spaces/Korla/hsb_stt_demo/tree/main'
+        )}
+        {getMenuPoint(
+          'Fraunhofer IKTS (homepage)',
+          'https://www.ikts.fraunhofer.de/de/industrieloesungen/akustische_diagnostik/bio_sprachsignale.html'
+        )}
+        {getMenuPoint('Wuwiće strony (homepage)', 'https://fs-coding.com')}
+      </Menu>
+      <Menu
+        id='basic-menu'
+        anchorEl={anchorElZorla}
+        open={openZorla}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button'
+        }}
+      >
+        {getMenuPoint('Whisper (github)', 'https://github.com/openai/whisper')}
+        {getMenuPoint('Whisper.cpp (github)', 'https://github.com/ggerganov/whisper.cpp')}
+        {getMenuPoint('Fairseq (github)', 'http://github.com/facebookresearch/fairseq')}
+        {getMenuPoint(
+          'Žórła aplikacije (github)',
+          'https://github.com/ZalozbaDev/uploader-recny-model/'
+        )}
+        {getMenuPoint(
+          'Žórła servera (github)',
+          'https://github.com/ZalozbaDev/uploader-recny-model-server/'
+        )}
+      </Menu>
+    </Box>
+  )
+}
